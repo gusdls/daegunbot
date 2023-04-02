6d3ef3a1a6625e65df2b50c7e616d5821daf2356
Dohyun Kim <0704dohyun@gmail.com>
2023-04-02 20:45:46 +0900

    feat(menu.py): sends menu that scraped

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from scraper.menu import scrape_menus
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -19,5 +21,21 @@ async def on_ready():
 @bot.command()
 async def ping(ctx: commands.Context):
     await ctx.send("pong!")
+
+@bot.command()
+async def menu(ctx: commands.Context):
+    embed = discord.Embed(
+        title="Daegun Menu Scraper",
+        description="It scrapes menus of DGHS",
+        color=discord.Color.random()
+    )
+    menu_cards = scrape_menus()
+    for menu_card in menu_cards:
+        embed.add_field(
+            name=menu_card.time,
+            value=os.linesep.join(menu_card.menus),
+            inline=True
+        )
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
---
+++ scraper/menu.py
@@ -0,0 +1,53 @@
+import re
+import datetime
+from dataclasses import dataclass
+from typing import Iterable
+
+import requests
+from bs4 import BeautifulSoup
+
+TODAY = datetime.date.today()
+DAYS_OF_A_WEEK = 7
+
+@dataclass
+class MenuCard:
+    time: str
+    menus: Iterable[str]
+    date: datetime.date
+
+    def check_date(self, date: datetime.date):
+        return self.date == date
+
+def check_weekend(date: datetime.date):
+    return date.isoweekday() > 5
+
+def extract_menu(section) -> MenuCard:
+    time = section.find('strong', class_='cm_date').get_text().strip()
+    month, day = map(int, re.findall("[0-9]{1,2}", time))
+    date = datetime.date(TODAY.year, month, day)
+    menus = section.find('div', class_='time_normal_list').find_all('span', class_='text')
+    menus = [menu.get_text() for menu in menus]
+
+    return MenuCard(time, menus, date)
+
+def scrape_menus() -> Iterable[MenuCard]:
+    menu_cards: Iterable[MenuCard] = []
+    url = "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=blBI&pkid=682&os=24929782&qvt=0&query=%EB%8C%80%EA%B1%B4%EA%B3%A0%EB%93%B1%ED%95%99%EA%B5%90%20%EA%B8%89%EC%8B%9D%EC%8B%9D%EB%8B%A8"
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.content, 'lxml')
+
+    target_date = TODAY if not check_weekend(TODAY) else TODAY + datetime.timedelta(days=DAYS_OF_A_WEEK-TODAY.weekday())
+    _, curr_week, next_week = soup.find_all('div', class_='timeline_list')
+    target_week = curr_week if not check_weekend(TODAY) else next_week
+    sections = target_week.find_all('div', class_='timeline_box')
+    for section in sections:
+        extracted_menu = extract_menu(section)
+        if extracted_menu.check_date(target_date):
+            menu_cards.append(extracted_menu)
+
+    return menu_cards
+
+if __name__ == "__main__":
+    for menu_card in scrape_menus():
+        print(menu_card)
